0dfd64be4331cd4905a6188ad9368042cb67e2a7
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-07 19:45:41 -0700

    8 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    ssize_t len = 59;
+    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    write(2, quote, len);
     return (1);
 }
