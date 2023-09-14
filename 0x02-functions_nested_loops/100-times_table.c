45d0c7544b070cf5686d86d67dd6fc6bd7ec7014
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-13 17:02:12 -0700

    function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_times_table - prints the n times table, starting with 0
@@ -16,12 +15,32 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 result = i * j;
+
 if (j == 0)
-printf("%d", result);
-else
-printf(", %d", result);
+_putchar(result + '0');
+else if (result < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(result + '0');
 }
-printf("\n");
+else if (result < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+}
+else
+{
+_putchar(',');
+_putchar((result / 100) + '0');
+_putchar(((result / 10) % 10) + '0');
+_putchar((result % 10) + '0');
+}
+}
+_putchar('\n');
 }
 }
 }
