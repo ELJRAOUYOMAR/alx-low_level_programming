a818ed937404fcb5dca80243efb224697bef43e9
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-16 10:04:15 -0700

    Lowercase Upercase except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 /**
-* main - Entry point , Lower and upper letters except q and e 
+* main - Entry point , Lower and upper letters except q and e
 *
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-char lowercase="a";
-while(lowercase<="z" && lowercase!="q" && lowercase!="e")
+char lowercase = 'a';
+while(lowercase <= 'z')
+{
+if (lowercase != 'e' && lowercase != 'q')
 {
 putchar(lowercase);
+}
 lowercase++;
 }
 putchar("\n");
 
-return(=)
+return(0);
 
 }
