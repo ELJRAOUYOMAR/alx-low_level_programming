6dafd5445dd42bfa066cc666bad925e14fb11bb8
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-16 10:23:01 -0700

    All single digits of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,17 +5,17 @@
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 int n = 0;
-while(n <= 9)
+
+while (n <= 9)
 {
-printf("%d",n);
+printf("%d" , n);
 n++;
 }
+
 printf("\n");
 
 return (0);
 }
-
