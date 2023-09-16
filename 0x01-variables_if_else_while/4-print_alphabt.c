b00e26f090fe24b39f8ab91c75379a051bd377d5
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-16 10:00:14 -0700

    Lowercase Upercase except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point , Lower and upper letters except q and e 
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char lowercase="a";
+while(lowercase<="z" && lowercase!="q" && lowercase!="e")
+{
+putchar(lowercase);
+lowercase++;
+}
+putchar("\n");
+
+return(=)
+
+}
