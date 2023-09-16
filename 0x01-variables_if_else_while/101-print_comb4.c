0345e21f121b8058fef0a7bb93a0e1d947f5d30a
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-16 12:27:47 -0700

    prints all possible different combinations of three digits with advanced feutures and exceptions

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,14 +3,14 @@
 /**
 * main - Entry point
 *
-*Numbers must be separated by ,, followed by a space
-*The three digits must be different
-*012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
-*Print only the smallest combination of three digits
-*Numbers should be printed in ascending order, with three digits
-*You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
-*You can only use putchar six times maximum in your code
-*You are not allowed to use any variable of type char
+* Description: Prints all possible different combinations of three digits.
+* Numbers must be separated by ', '.
+* The three digits must be different.
+* 012, 120, 102, 021, 201, 210 are considered the same combination.
+* Print only the smallest combination of three digits.
+* Numbers should be printed in ascending order, with three digits.
+* You can only use the putchar function and a maximum of six times.
+* You are not allowed to use any variable of type char.
 *
 * Return: Always 0 (Success)
 */
@@ -23,7 +23,7 @@ for (first_digit = 0; first_digit <= 7; first_digit++)
 for (second_digit = first_digit + 1; second_digit <= 8; second_digit++)
 {
 for (third_digit = second_digit + 1; third_digit <= 9; third_digit++)
-{     
+{
 putchar(first_digit + '0');
 putchar(second_digit + '0');
 putchar(third_digit + '0');
