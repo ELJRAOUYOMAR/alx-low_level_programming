4e8c85d5ecfe531800797597ab93b5e1e27374bd
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-16 08:38:09 -0700

    Positive or negative exercice

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point, generates and classifies a random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+/* Initialize random number generator */
+srand(time(0));
+
+/* Generate a random number between INT_MIN and INT_MAX */
+n = rand();
+
+/* Check if n is positive, negative, or zero, and print the result */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+
+return (0);
+}
