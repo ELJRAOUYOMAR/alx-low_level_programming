2d006964dfb92a27094691a5db05b3ba168aa8c1
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-17 05:37:02 -0700

    Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2,

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
-int main() {
-    long int prev = 1;
-    long int current = 2;
-    long int sum = 0;
+/**
+* main - Entry point for the Fibonacci program.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int n1 = 1, n2 = 2, next, sum = 0;
 
-    while (current <= 4000000) {
-        if (current % 2 == 0) {
-            sum += current;
-        }
-
-        long int next = prev + current;
-        prev = current;
-        current = next;
-    }
-
-    printf("%ld\n", sum);
-
-    return 0;
+while (n2 <= 4000000)
+{
+if (n2 % 2 == 0)
+{
+sum += n2;  // Add even-valued term to the sum
 }
+
+// Calculate the next Fibonacci term
+next = n1 + n2;
+n1 = n2;
+n2 = next;
+}
+
+printf("The sum of even-valued terms in the Fibonacci sequence that do not exceed 4,000,000 is: %d\n", sum);
+
+return (0);
+}
+
