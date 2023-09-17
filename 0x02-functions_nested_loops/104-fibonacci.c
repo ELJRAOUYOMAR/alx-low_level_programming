06ef0b1032165b27fe88ab62c991f5a4253fa9fb
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-17 05:25:49 -0700

    98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints the first 98 Fibonacci numbers separated by comma and space.
- * The numbers should be separated by a comma, followed by a space.
- * You are allowed to use the standard library.
- * You are not allowed to use any other library (You can’t use GMP, etc…).
- * You are not allowed to use long long, malloc, pointers, arrays/tables, or structures.
- * You are not allowed to hard code any Fibonacci number (except for 1 and 2).
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    unsigned long long int a = 1, b = 2;
-    int count = 2; /* Start with 2 since we already have 1 and 2 */
+unsigned long a = 1, b = 2, c;
+int count = 0;
 
-    printf("%llu, %llu, ", a, b);
+while (count < 98)
+{
+if (count == 0)
+printf("%lu, %lu, ", a, b);
+else if (count != 97)  /* Print the comma and space */
+printf("%lu, ", c);
+else                  /* Print the last number with a newline */
+printf("%lu\n", c);
 
-    while (count < 98)
-    {
-        /* Calculate the next Fibonacci number */
-        unsigned long long int next = a + b;
+/* Calculate the next Fibonacci number */
+c = a + b;
+a = b;
+b = c;
+count++;
+}
 
-        /* Print the number with comma and space */
-        printf("%llu, ", next);
-
-        /* Update variables for the next iteration */
-        a = b;
-        b = next;
-        count++;
-    }
-
-    printf("\n");
-
-    return (0);
+return (0);
 }
