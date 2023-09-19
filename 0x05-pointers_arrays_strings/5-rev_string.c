d66a17e15e0deb730f881ee84bef63275484a29c
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-19 10:47:56 -0700

    unction that reverses a string.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* rev_string - Reverse a string in place.
+* @s: The input string to reverse.
+*/
+void rev_string(char *s)
+{
+if (s == NULL)
+return;
+
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+
+int i;
+for (i = 0; i < length / 2; i++)
+{
+/* Swap characters from the beginning and end of the string */
+char temp = s[i];
+s[i] = s[length - 1 - i];
+s[length - 1 - i] = temp;
+}
+}
+
