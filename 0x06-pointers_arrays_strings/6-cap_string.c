008104aa69c2c92fefcaabf0d3ae68d2562273de
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-21 08:30:05 -0700

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,55 +1,31 @@
 #include "main.h"
 
 /**
-* is_separator - Checks if a character is a separator.
-* @c: The character to check.
-*
-* Return: 1 if c is a separator, 0 otherwise.
-*/
-int is_separator(char c)
-{
-char separators[] = " \t\n,;.!?\"(){}";
-int i;
-
-for (i = 0; separators[i] != '\0'; i++)
-{
-if (c == separators[i])
-{
-return 1;
-}
-}
-
-return 0;
-}
-
-/**
-* cap_string - Capitalizes all words in a string.
+* cap_string - Capitalize all words of a string.
 * @str: The input string.
 *
 * Return: A pointer to the modified string.
 */
 char *cap_string(char *str)
 {
-int i = 0;
+int i;
 
-while (str[i] != '\0')
+if (str[0] >= 'a' && str[0] <= 'z')
+str[0] -= 32;  /* Capitalize the first character */
+
+for (i = 1; str[i] != '\0'; i++)
 {
-if (is_separator(str[i]))
+if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+str[i - 1] == '}') &&
+(str[i] >= 'a' && str[i] <= 'z'))
 {
-i++; /* Skip separators */
-}
-else if (str[i] >= 'a' && str[i] <= 'z') /* Check if lowercase letter */
-{
-/* Convert to uppercase by subtracting the ASCII difference */
-str[i] = str[i] - 'a' + 'A';
-i++;
-}
-else
-{
-i++; /* Skip non-lowercase letters */
+str[i] -= 32;  /* Capitalize the current character */
 }
 }
 
-return str;
+return (str);
 }
 
