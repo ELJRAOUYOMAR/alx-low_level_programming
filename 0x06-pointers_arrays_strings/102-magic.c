bb2f411b4bf3448deb335831aa8e9e3cff849528
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-21 12:36:38 -0700

    Add one line to https://github.com/alx-tools/make_magic_happen/blob/master/magic.c, so that the program prints a[2] = 98, followed by a new line.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
 int main(void)
 {
 int n;
@@ -14,16 +9,15 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* Add one line of code here...
+* write your line of code here...
 * Remember:
-* - You are not allowed to use 'a'
-* - You are not allowed to modify 'p'
-* - Only one statement
-* - You are not allowed to code anything else than this line of code
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
 */
-*(p + 5) = 98;  /* This line sets n to 98 without using 'a' or modifying 'p' */
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
-
