429fa9d26cd13682e4a041506affa7b4b911e147
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-21 03:16:52 -0700

    function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* Description - The standard library provides a similar function: strcmp. Run man strcmp to learn more.
+*
+* Return: 0 if the strings are equal, a positive value if s1 > s2, and a negative value if s1 < s2.
+*/
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && *s2 && *s2 == *s1)
+{
+s1++;
+s2++;
+}
+
+/* Return value: 0 if equal, positive if s1 > s2, negative if s1 < s2 */
+return *s1 - *s2;
+}
