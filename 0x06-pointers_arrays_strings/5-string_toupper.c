8ea33798416efc093087191b88060f141d2c8716
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-21 06:32:51 -0700

    function that changes all lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -17,9 +17,6 @@ if (str[i] >= 'a' && str[i] <= 'z')
 {
 /* Convert to uppercase by subtracting the ASCII difference */
 str[i] = str[i] - ('a' - 'A');
-
-/* other method : Subtracting 32 to convert lowercase to uppercase in ASCII */
-/* str[i] = str[i] - 32; */
 }
 i++;
 }
