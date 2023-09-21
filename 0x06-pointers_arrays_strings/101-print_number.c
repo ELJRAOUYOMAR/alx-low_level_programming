7b5317fc0d871fd2ad4e6c7bc56f406772c3e164
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-21 11:41:43 -0700

    function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
-* print_number - Prints an integer.
-* @n: The integer to be printed.
+* print_number - Prints an integer
+* @n: The integer to be printed
 */
 void print_number(int n)
 {
 if (n < 0)
 {
-_putchar('-');
-n = -n;  /* Make n positive */
+putchar('-');
+n = -n;
 }
 
 if (n / 10 != 0)
-{
-print_number(n / 10); /* Recursively print the higher-order digits */
-}
+print_number(n / 10);
 
-_putchar((n % 10) + '0'); /* Print the last digit */
+putchar((n % 10) + '0');
 }
 
