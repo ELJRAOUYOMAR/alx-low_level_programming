696b7bc23dca19d478afe516b159cff1b5bf92cf
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-24 09:14:40 -0700

    debugging positive_or_negative() function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,35 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Test function for positive or negative
- * return - 0
+ * main - Entry point, generates and classifies a random number
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int i ;
-i=0;
-positive_or_negative(i);
-return 0;
+int n;
+
+/* Initialize random number generator */
+srand(time(0));
+
+/* Generate a random number between INT_MIN and INT_MAX */
+n = rand() - RAND_MAX / 2;
+
+/* Check if n is positive, negative, or zero, and print the result */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+
+return (0);
 }
