3aeb93637f1d62957f022324f6289e6480a6c3c6
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-25 11:22:14 -0700

    function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,29 +1,23 @@
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string
- * @s: Pointer to the string
- * @c: The character to locate
- *
- * Return: A pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found.
- */
+* _strchr - Locates a character in a string.
+* @s: Pointer to the string to search in
+* @c: The character to locate
+*
+* Return: A pointer to the first occurrence of the character c in the string s,
+* or NULL if the character is not found.
+*/
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-        {
-            return s;
-        }
-        s++;
-    }
-
-    if (c == '\0')
-    {
-        return s;
-    }
-
-    return NULL;
+int i ;
+for (i = 0; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+{
+return (s + i) ;
+}
 }
 
+return ('\0');
+}
