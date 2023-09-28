fa9dc75dded5b4c61f587b83f05d9156e6f86904
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-09-28 16:01:05 -0700

    function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,51 +1,30 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number for which to find the square root.
+ * _sqrt_recursion - Calculates the natural square root of a number
+ * @n: The number to find the square root of
  *
- * Return: The natural square root of n.
- *         If n does not have a natural square root, return -1.
+ * Return: The natural square root of the number, or -1 if not found
  */
 int _sqrt_recursion(int n)
 {
-    /* Base case: if n is negative, there is no natural square root */
-    if (n < 0)
-    {
-        return -1;
-    }
-
-    /* Base case: if n is 0 or 1, the square root is n itself */
-    if (n == 0 || n == 1)
-    {
-        return n;
-    }
-
-    /* Recursive case: Check for the square root in the range [1, n/2] */
-    int result = check_square_root(n, 1);
-    return result;
+    return find_sqrt(n, 1);
 }
 
 /**
- * check_square_root - A recursive function to perform binary search for the square root.
- * @n: The number for which to find the square root.
- * @i: The current search value.
+ * find_sqrt - Recursive helper function to find the square root
+ * @n: The number to find the square root of
+ * @i: The current guess for the square root
  *
- * Return: The natural square root of n.
- *         If n does not have a natural square root, return -1.
+ * Return: The natural square root of the number, or -1 if not found
  */
-int check_square_root(int n, int i)
+int find_sqrt(int n, int i)
 {
     if (i * i == n)
-    {
-        return i;  /* Found the square root */
-    }
+        return i;
     else if (i * i > n)
-    {
-        return -1;  /* Square root cannot be found in the remaining range */
-    }
-
-    /* Recursive case: continue the search */
-    return check_square_root(n, i + 1);
+        return -1;
+    else
+        return find_sqrt(n, i + 1);
 }
 
