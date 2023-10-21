961ddb075d1eb3553dd1bcc99d1f4520fed56543
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-10-21 04:43:29 -0700

    remove folders of the #month1

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compile all .c files in the current directory into corresponding .o files
-gcc -c *.c
-
-# Create a static library named liball.a from all the generated .o files
-ar -rcs liball.a *.o
