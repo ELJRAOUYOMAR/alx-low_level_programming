183f59b0de363fdc45b9b6ec07211b3f287d8d8e
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-10-29 15:16:05 -0700

    main.h file README file _putchar file and the first exercice

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+    (void)argc;
+    printf("%s\n", argv[0]);
+    return (0);
+}
