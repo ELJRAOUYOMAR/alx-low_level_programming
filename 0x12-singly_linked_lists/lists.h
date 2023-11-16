12d371d6b3906af6819eceb8e06d8e4d19385155
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-11-16 22:56:24 +0100

    Edit Header File

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
-#ifndef LINKED
-#define LINKED
+#ifndef _LISTS_
+#define _LISTS_
 
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -25,3 +25,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+#endif
