af8a17e2e299cee00dbf0d546dfdee9045fb3c4b
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-12-01 13:24:22 -0800

    Exercice 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,38 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * create_file - Creates a file with the given content
- * @filename: The name of the file to create
- * @text_content: The content to write to the file
- *
- * Return: 1 on success, -1 on failure
- */
+* create_file - creates a file
+* @filename: filename.
+* @text_content: content writed in the file.
+*
+* Return: 1 if it success. -1 if it fails.
+*/
 int create_file(const char *filename, char *text_content)
 {
-    int fd, len, written;
+int fd;
+int nletters;
+int rwr;
 
-    if (filename == NULL)
-        return (-1);
+if (!filename)
+return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (text_content == NULL)
-    {
-        close(fd);
-        return (1);
-    }
+if (fd == -1)
+return (-1);
 
-    for (len = 0; text_content[len] != '\0'; len++)
-        continue;
+if (!text_content)
+text_content = "";
 
-    written = write(fd, text_content, len);
-    close(fd);
+for (nletters = 0; text_content[nletters]; nletters++)
+;
 
-    return (written == len ? 1 : -1);
+rwr = write(fd, text_content, nletters);
+
+if (rwr == -1)
+return (-1);
+
+close(fd);
+
+return (1);
 }
-
